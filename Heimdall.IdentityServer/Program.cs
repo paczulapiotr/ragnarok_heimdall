3b0aa2e925d3d779ebaab2bce2a7fa747842f833
paczulapiotr <paczulapiotr@gmail.com>
2019-07-14 19:50:02 +0200

    Sample IS4 project

---
+++ Heimdall.IdentityServer/Program.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var seed = args.Any(x => x == "/seed");
+            if (seed) args = args.Except(new[] { "/seed" }).ToArray();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            if (seed)
+            {
+                using (var scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    SeedData.EnsureSeedData(scope.ServiceProvider);
+                    return;
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>();
+        }
+    }
+}
