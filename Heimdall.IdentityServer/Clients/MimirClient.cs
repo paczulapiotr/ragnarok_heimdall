6d6c902d8f032fcde17c711a8b9b23033486bdf8
paczulapiotr <paczulapiotr@gmail.com>
2019-12-09 19:03:33 +0100

    Fix mimir client url

--- Heimdall.IdentityServer/Clients/MimirClient.cs
+++ Heimdall.IdentityServer/Clients/MimirClient.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Linq;
 
 namespace Heimdall.IdentityServer.Clients
@@ -13,18 +15,23 @@ namespace Heimdall.IdentityServer.Clients
     {
         private readonly IHttpClientFactory _clientFactory;
         private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _environment;
 
-        public MimirClient(IHttpClientFactory clientFactory, IConfiguration configuration)
+        public MimirClient(IHttpClientFactory clientFactory, IConfiguration configuration, IWebHostEnvironment environment)
         {
             _clientFactory = clientFactory;
             _configuration = configuration;
+            _environment = environment;
         }
 
         public async Task<bool> CreateUserAsync(string authId, string name)
         {
             using (var client = _clientFactory.CreateClient())
             {
-                var request = new HttpRequestMessage(HttpMethod.Put, $"{_configuration["Security:ApiUrl"]}/api/user/create");
+                var urlRoot = _environment.IsDevelopment()
+                    ? _configuration["Security:Local:ApiUrl"]
+                    : _configuration["Security:Azure:ApiUrl"];
+                var request = new HttpRequestMessage(HttpMethod.Put, $"{urlRoot}/api/user/create");
                 request.Content = new StringContent(
                     JObject.FromObject(new { authId, name }).ToString(),
                     Encoding.UTF8,
